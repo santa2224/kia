963000245e86f8fa83a60b247c8b2cb9f9b19d7a
santa2224 <64447885+santa2224@users.noreply.github.com>
2020-05-20 13:04:23 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(function(){
     // 스킵메뉴
-    $('.skip_menu>p>a').focus(function(){
-      $('.skip_menu').addClass('active');
+    $('.skip_menu>a').focus(function(){
+      $('.skip_menu').animate({top:'0px'},300)
     });
-    $('.skip_menu>p>a').blur(function(){
-      $('.skip_menu').removeClass('active');
+    $('.skip_menu>a').blur(function(){
+      $('.skip_menu').animate({top:'-43px'},300)
     });
   })
